4dfcd16c10ff1e2ff39906f6a7d31c5d6f4175d3
Vladimir Popinov <Vladimir.popinov@gmail.com>
2017-11-11 18:25:49 +0300

    Add files via upload

--- js/views/main.js
+++ js/views/main.js
@@ -98,26 +98,45 @@ function userRegistrationForm(){
 function drawAdminProfile(){
 
   $('body').empty();
+  $('body').css("background-color","#2f343a");
 
-  var nav = $('<div class="header2">')
+  var nav = $('<ul class="header2">').html('<li class="header2_left"><span class="header2_admin">Admin</span></li>    <li style><span class="header2_panel">Панель</span></li>    <li style="margin-top:15px;"class="dropdown">      <button class="dropbtn">Выбрать категорию<span class="header2_select__arrow"/></button>      <div class="dropdown-content">        <a href="#" onclick="drawFilmRedactor()">Редактировать фильмы</a>        <a href="#">Управлять бронированием</a>        <a href="#">Пользователи</a>   <a href="#">Статистика</a>     </div>    </li> <li class="header2_right"><button type="button" class="btn adminMenuButton">Выход</button></li>');
+var layout = $('<div class="layout">').append($('<div class="content">').append($('<h2>').html('Краткий отчет об ошибках')));
+  /*var nav = $('<div class="header2">')
     .append($('<div class="header2_cell header2_left">')
       .append($('<span class="header2_admin">')
         .html('Admin'))
       .append($('<span class="header2_panel">')
         .html('Панель')))
     .append($('<div class="header2_cell header2_middle">')
-      .append($('<select class="header2_select">')
-        .append($('<option class="selectOption">')
-          .html('Редактирование фильмотеки'))
-        .append($('<option class="selectOption">')
-          .html('Управление бронированием'))
-        .append($('<option class="selectOption">')
-          .html('Просмотр активности пользователей'))
-        .append($('<option class="selectOption">')
-          .html('Статистика'))))
+      .append($('<div class="header2_select dropdown">')
+          .append($('<div class="header2_select__arrow">'))
+          .append($('<div class="dropdown">')
+              .html($('<button class="dropbtn"><span>wewef</span></button>'))
+            .append($('<div class="dropdown-content">')
+              .append($('<a href="#">')
+                .html($('Link 1'))))
+            .append($('<div class="dropdown-content">')
+              .append($('<a href="#">')
+                .html($('Link 2'))))
+            .append($('<div class="dropdown-content">')
+              .append($('<a href="#">')
+                .html($('Link 3')))))))
     .append($('<div class="header2_cell header2_right">')
-      .append($('<span class="header2_logout">')
-        .html('Выход')));
-
-    $('body').append(nav);
+      .append($('<button type="button" class="btn adminMenuButton">')
+        .append($('<span class="header2_select__arrow">'))
+        .html('Выход')));*/
+    $('body').append(nav).append(layout);
 };
+
+function drawAdminNav(title){
+  $('body > ul > li.dropdown > button').text(title).append('<span class="header2_select__arrow"/>');
+}
+
+function drawFilmRedactor(){
+  $('body .layout').empty();
+
+  drawAdminNav('Редактор фильмов');
+
+  $('.layout').append('<div class="filmRedactorMenu"><li class="list-header">Категории</li><li class="selected"> <span onclick="drawFilmRedactor()">Добавить сеанс</span></li><li>        <span>Удалить сеанс</span></li><li>        <span>Добавить фильм</span></li><li>   <span>Удалить фильм</span></li></div><div class="right"><div><li class="list-header"><span style="margin:20px;">Добавление сеанса</span><li></div></div>')
+}
