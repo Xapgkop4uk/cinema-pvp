783db1a4e496b8f4cd9b730db21050674880aa13
Vladimir Popinov <Vladimir.popinov@gmail.com>
2017-11-30 17:58:36 +0300

    release about movie and adding comment to film

--- js/views/film.js
+++ js/views/film.js
@@ -1,11 +1,27 @@
+var chooseStars = 0;
 function aboutFilm(id){
   $.ajax({
     url:"https://api.backendless.com/"+APP_ID+"/"+API_KEY+"/data/movies/"+id,
     type: "GET",
     success: function(result){
+      $.ajax({
+        url:"https://api.backendless.com/"+APP_ID+"/"+API_KEY+"/data/comments?where=movie%3D'"+id+"'",
+        type: "GET",
+        success: function(result){
+          result.forEach((element)=>{
+            $('.comments-list').append($('<div class="comment-body">')
+              .append($('<span>').html(element.username))
+              .append($('<span>').html(element.mark+"/10"))
+              .append($('<div class="red-line">'))
+              .append($('<div>').html(element.comment)));
+          })
+        }
+      });
+
+
       console.log(result);
       $('body').append($('<div id="CinemaModal" style="z-index:100;" class="infoModal">')
-        .append($('<div class="modal-content" style="padding:0px; margin:0 auto; border:1px solid #5d0505">')
+        .append($('<div class="modal-content" style="padding:0px; margin:20px auto; border:1px solid #5d0505">')
           .append($('<div id="Сontent" style="display:inline-flex; margin:20px">')
             .append($('<div style="width:320px; text-align:center;">')
               .html('<img class="poster" style="box-shadow: 0px 8px 16px 2px #5d0505;" src="https://api.backendless.com/F4938450-8412-F432-FF30-7FF933EE1300/9D5C7C66-9B9D-35B7-FF7F-5EB8144C5C00/files/images/'+result.objectId+'.jpg"/>'))
@@ -38,10 +54,64 @@ function aboutFilm(id){
               .append($('<div class="group about-group">')
                 .append($('<p class="property" >')
                   .html('Рейтинг: ')
-                .append($('<t class="prop-value">')
+                .append($('<t class="prop-value" id="mark">')
                   .html(result.mark+"/10"))))))));
-        $('.modal-content').append($('<div style="padding:20px;">')
-          .html(result.description));
+        $('.modal-content').append($('<div style="margin:20px;">')
+          .html(result.description))
+        .append($('<div class="red-line">'));
+
+        if(getCookie('username') == undefined){
+          $('.modal-content').append($('<div class="comment-box">')
+            .html('Для оценки и комментирования необходима аутентификация.'));
+        }
+        else {
+          var stars = $('<div class="star-selection">');
+          stars.append($('<span class="title">').html('Оценить: '))
+
+          var commentBox = $('<div class="group">')
+            .append($('<textarea id="comment" "class="textEditArea" name="description" placeholder="Комментарий" style="background:rgba(93,5,5,0.5); display:unset;width:unset;font-size:unset;max-width:unset;min-width:unset;min-width:460px;max-width:460px; color:white;" rows="5" cols="60" >'))
+            .append($('<div style="height:50px;">').append($('<button class=" session-btn" style="position:absolute;right:40px;" onclick="postComment(\''+result.objectId+'\')" type="button">')
+              .append($('<span>')
+                .html('Опубликовать'))));
+
+          for(var i =0; i < 10;i++){
+            stars.append($('<i class="material-icons star" id="'+i+'-star">').html('star_border'));
+          }
+          $('.modal-content')
+          .append($('<h3>').html('Добавить комментарий'))
+          .append($('<div class="comment-box">')
+            .append(stars)
+            .append(commentBox));
+
+        }
+
+        $('.modal-content')
+        .append($('<div class="red-line">'))
+        .append($('<h3>').html('Отзывы'));
+        .append($('<div class="comments-list">'));
+
+
+
+
+        $('.star').click((e)=>{
+          chooseStars = e.target.id[0];
+          $('.star').each((index, element)=>{
+              if(chooseStars >= element.id[0]){
+                $(element).html('star');
+              }
+              else{
+                $(element).html('star_border');
+              }
+          });
+        });
+
+        $('.modal-content').click(function(e){
+          e.stopPropagation();
+        });
+
+        $('#CinemaModal').on('click', function (e) {
+          $('#CinemaModal').remove();
+        });
       }
   });
 }
@@ -91,7 +161,6 @@ function drawSessionsSelection(movie){
               }
               $('.sessions-list').last()
               .append($('<button class="session-btn"  onclick="drawSelectedSession(\''+element.movie+'\',\''+element.objectId+'\')">').html(addZero(date.getHours())+':'+addZero(date.getMinutes())));
-
           }
         });
       }
@@ -186,4 +255,53 @@ function buyTickets(id)
         }
       });
     });
+}
+
+function postComment(movieId){
+  var json={
+    movie: movieId,
+    userId:getCookie('userId'),
+    username:getCookie('username'),
+    mark:chooseStars,
+    comment:$('#comment').val()
+  }
+
+  $.ajax({
+    url:"https://api.backendless.com/"+APP_ID+"/"+API_KEY+"/data/comments",
+    contentType:"application/json",
+    type: "POST",
+    dataType: "json",
+    data: JSON.stringify(json),
+    success: function(d){
+      $.ajax({
+        url:"https://api.backendless.com/"+APP_ID+"/"+API_KEY+"/data/comments?where=movie%3D'"+movieId+"'",
+        type: "GET",
+        success: function(result){
+          var count = 0;
+          var mark = 0;
+          result.forEach((element)=>{
+            mark += parseInt(element.mark);
+            count++;
+          });
+          value = parseInt((mark/count).toFixed(2));
+          console.log(value);
+          var updateMark ={
+            mark: value,
+            marks: count
+          }
+
+          $.ajax({
+            url:"https://api.backendless.com/"+APP_ID+"/"+API_KEY+"/data/movies/"+movieId,
+            contentType:"application/json",
+            type: "PUT",
+            dataType: "json",
+            data: JSON.stringify(updateMark),
+            success: function(d){
+              $('#mark').html(value);
+            }
+          });
+        }
+      });
+    }
+  });
 }
